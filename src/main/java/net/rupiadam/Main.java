17f953485ad5026d7a458e28cc1c7735adf26874
Rupi <rupi.adam@gmail.com>
2024-07-17 16:41:32 +0700

    feat: add sample of implementation array sum function
    - Added VCS configuration for Git in project settings
    - Implemented a function to calculate cumulative sums of an integer array
    - Updated unit tests to validate the sum function with multiple inputs

--- src/main/java/net/rupiadam/Main.java
+++ src/main/java/net/rupiadam/Main.java
@@ -1,7 +1,22 @@
 package net.rupiadam;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var result = sum(new int[]{1, 2, 3, 4});
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] sum(int[] nums) {
+        int[] result = new int[nums.length];
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            result[i] = currentSum;
+        }
+
+        return result;
     }
 }
--- src/test/java/net/rupiadam/MainTest.java
+++ src/test/java/net/rupiadam/MainTest.java
@@ -2,12 +2,28 @@ package net.rupiadam;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MainTest {
 
     @Test
-    void main() {
-        assertEquals("Hello world!", "Hello world!");
+    void firstSum() {
+        int[] firstInput = {1, 2, 3, 4};
+        int[] firstOutput = {1, 3, 6, 10};
+        assertArrayEquals(firstOutput, Main.sum(firstInput));
+    }
+
+    @Test
+    void secondSum() {
+        int[] secondInput = {1, 1, 1, 1, 1};
+        int[] secondOutput = {1, 2, 3, 4, 5};
+        assertArrayEquals(secondOutput, Main.sum(secondInput));
+    }
+
+    @Test
+    void thirdSum() {
+        int[] thirdInput = {3, 1, 2, 10, 1};
+        int[] thirdOutput = {3, 4, 6, 16, 17};
+        assertArrayEquals(thirdOutput, Main.sum(thirdInput));
     }
 }
